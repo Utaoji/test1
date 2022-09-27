efcf390d3dd41cd5050a620db53c04bc1a6ba6fc
Utaoji <113982667+Utaoji@users.noreply.github.com>
2022-09-27 23:41:47 +0900

    initial commit

---
+++ 2022mita/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>小原好美のココちゃんねる～ココロの中を語ってみた！～</title>
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../style.css">
+    <script src="../../js/jquery-3.1.1.min.js"></script>
+    <script src="../../js/bootstrap.min.js"></script>
+</head>
+<body>
+<header class="content-header">
+<div class="header-inner">
+    <div class="header-site">
+    <h1><a href="../../"><img src="../../img/logo_s.png" alt="こえけん！アニメ声優研究会" width="200" height="100"></a></h1>
+    </div> 
+    <h1 class="event-title">2022年度三田祭開催イベント</h1>
+</div> 
+</header>
+<main>
+    <article class="event-special">
+        <div class="event-logo">
+            <img src="title_ori.png" alt="小原好美のココちゃんねる～ココロの中を語ってみた！～" width="80%" height=auto>
+        
+        <h2>イベント概要</h2>
+        <table>
+            <tr><th>タイトル</th><td>小原好美のココちゃんねる～ココロの中を語ってみた！～</td></tr>
+            <tr><th>開催日時</th><td>2022年11月20日(日)<br>開場 15:45 / 開演 16:00</td></tr>
+            <tr><th>開催場所</th><td>慶應義塾大学 三田キャンパス 西校舎ホール<br>東京都港区三田2-15-45</td></tr>
+            <tr><th>入場方法</th><td>未定</td></tr>
+            <tr><th>主催</th><td><a href="https://www.keiokoeken.com/" class="ffflnk" target="_blank">慶應義塾大学公認サークル こえけん！アニメ声優研究会</a></td></tr>
+            <tr><th>出演者</th><td>小原好美</td></tr>
+            <tr><th>内容</th><td>慶應義塾大学三田祭において、声優さんをお招きして開催するトークショー形式のイベントです。<br>2022年度三田祭では現在声優として活躍なさっている小原好美さんをお呼びしてトークショーを行います。</td></tr>
+        </table>
+        <h2>出演者プロフィール</h2>
+        <div class="profile">
+            <img src="koharakonomi.jpg" alt="" width="400" class="profile-img">
+            <div class="profile-text">
+                <h3>小原好美</h3>
+                <p> 大沢事務所所属。</p>
+                <h4>主な出演作品</h4>
+                <ul>
+                    <li>未定</li>
+                </ul>
+            </div>
+        </div>
+        
+        <p><h3>注意事項</h3></p>
+        <ul>
+            <li>未定</li>
+        </ul>  
+
+        <div id="enquetes">  
+    <!--<h2>アンケート</h2>
+            <h3>質問箱</h3>
+            <p>本イベント内コーナー企画で扱う小原好美さんへの質問・お悩みを募集しております！ぜひご応募ください！</p>
+            <a href=""><p>質問箱はこちら</p></a>
+            <h3>トークテーマ募集</h3>
+            <p>本イベント内コーナー企画で扱う小原好美さんに語ってほしいテーマを募集しております！ぜひご応募ください！</p>
+            <a href=""><p>トークテーマ応募はこちら</p></a>
+        </div>
+        <h2>アンケート</h2>
+        <p>本イベントのアンケートにご協力ください！アンケートフォームは<a href="">こちら</a>
+        <br> ※イベント当日(11月20日)の16:00よりアンケートへ回答できます
+        </p>-->
+        <h2>アクセス</h2>
+        <p>慶應義塾大学 三田キャンパス<br>東京都港区三田2-15-45</p>
+        <div class="ggmap">
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3242.1382924654135!2d139.74074355111276!3d35.648964280104714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bb019af22ef%3A0x5df1bbe84d05a9d4!2sKeio+University!5e0!3m2!1sen!2sjp!4v1508306192567" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+	</div>
+        <h2>お問い合わせ</h2>
+        <p>何か有りましたら以下のメールアドレスまでお問い合わせください。(画像クリックでメールを送信できます)</p>
+<!--メールアドレス暗号化のおまじない-->
+<script type="text/javascript">
+<!--
+function converter(M){
+var str="", str_as="";
+for(var i=0;i<M.length;i++){
+str_as = M.charCodeAt(i);
+str += String.fromCharCode(str_as + 1);
+}
+return str;
+}
+function mail_to(k_1,k_2)
+{eval(String.fromCharCode(108,111,99,97,116,105,111,110,46,104,114,101,102,32,
+61,32,39,109,97,105,108,116,111,58) 
++ escape(k_1) + 
+converter(String.fromCharCode(108,104,115,96,49,47,49,48,63,106,100,104,110,106,110,100,106,100,109,45,98,110,108,
+62,114,116,97,105,100,98,115,60)) 
++ escape(k_2) + "'");} 
+document.write('<a href=JavaScript:mail_to("","")><img src="2022mita_mail.png" width="80%" height=auto><\/a>');
+//-->
+</script>
+<noscript></noscript>
+</main>
+<footer>
+<div class="footer-inner">
+    <div class="copyright">
+    <p>&copy; 2016-2022 こえけん！アニメ声優研究会</p>
+    </div>
+</div>
+</footer>
+</body>
+</html>
